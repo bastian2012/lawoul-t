ba4ad802ea268f983b968ad52a5ca9bf6beb612a
Bastien Jean Ritchy <bjeanritchy@gmail.com>
2023-10-29 19:55:50 -0400

    affichage version 1

--- lawoulet.py
+++ lawoulet.py
@@ -1,5 +1,5 @@
 import random,os,pickle
-nomb_user=0;max=50;min=0;nouvo_sko=0
+max=50;min=0;nouvo_sko=0;ansyen_sko=0
 def test_maj(chaine):
     for i in chaine:
         if i.isupper():
@@ -21,23 +21,34 @@ def test_non():
 
 non_itilizatè=test_non()
 file_name='file.pk1'
-try:
+
+def lecture():
     with open(file_name, 'rb') as fichier:
         data = pickle.load(fichier)
+    return(data)
+def ecriture(file_name,data):
+    with open(file_name, 'wb') as fichier:
+        pickle.dump(data, fichier)
+
+def clear():
+    os.system("clear")
+
+def eta_jwèt(ansyen_sko,sko,nouvo_sko):
+    print(f"\nNon itilizatè : {non_itilizatè} \tansyen sko :{ansyen_sko}\t\t sko pati a : {sko}\t\t nouvo sko : {nouvo_sko}")
+
+
+try:
+    data = lecture()
 except (FileNotFoundError, EOFError):
     data = {}
-with open(file_name, 'wb') as fichier:
-    pickle.dump(data, fichier)
+ecriture(file_name,data)
     
-with open(file_name, 'rb') as fichier:
-        data = pickle.load(fichier)
+data=lecture()
         
 if not non_itilizatè in data:
     data[non_itilizatè]=0
-    with open(file_name, 'wb') as fichier:
-        pickle.dump(data, fichier)
-with open(file_name, 'rb') as fichier:
-    data = pickle.load(fichier)
+    ecriture(file_name,data)
+data=lecture()
 
 while True:
     nomb_ordi=5
@@ -45,32 +56,39 @@ while True:
     ansyen_sko=data[non_itilizatè]
     nouvo_sko=ansyen_sko
     while chans>0:
+        if chans==5:
+            eta_jwèt(ansyen_sko,score,nouvo_sko)
         try:
             nomb_user=int(input(f" {non_itilizatè} antre yon nomb ant {min} ak {max} :"))
-            os.system("clear")
             if nomb_user>=min and nomb_user <= max:
                 tantativ+=1;chans-=1
                 if nomb_user==nomb_ordi:
+                    clear()
                     if chans==0:
                         score=30
                     else:
                         score +=30*(chans)
                     nouvo_sko +=score
                     data[non_itilizatè]=nouvo_sko
-                    with open(file_name,'wb') as fichier:
-                        pickle.dump(data,fichier)
-
-                    print(f"bravo {non_itilizatè} !!!\n\n ou gnyn sou {tantativ} tantativ ou fè yon sko {score} pwen nouvo sko a se {nouvo_sko}\n\n")
+                    ecriture(file_name,data)
+                    eta_jwèt(ansyen_sko,score,nouvo_sko)
+                    print(f"bravo {non_itilizatè} !!!\n\n ou gnyn sou {tantativ} tantativ ou fè yon sko {score} pwen nouvo sko a se {data[non_itilizatè]}\n\n")
                     break
                 else:
+                    clear()
+                    eta_jwèt(ansyen_sko,score,nouvo_sko)
                     if nomb_ordi<nomb_user:
                         print(f"ou pèdi {non_itilizatè} !!\n sa ou chwazi a pi gro pase nomb ki soti nan tiraj la\nou rete {chans} chans. sko ou se {score} pwen\n")
                     else:
                         print(f" {non_itilizatè} ou pèdi !!\n\n sa ou chwazi a pi piti pase nomb ki soti nan tiraj la\n\nou rete {chans} chans. sko ou se {score} pwen\n\n")
+            else:
+                clear()
+                
         except:
             os.system("clear")
             print("sa ou antre a pa yon nomb !!!\n")
-    kontinye=input(f"\n{non_itilizatè} peze K pou'w soti nan jwèt la | nenpot lot touch pou'w kontinye  :")
+    print(f"\n {non_itilizatè} nomb kache a se te {nomb_ordi} ")
+    kontinye=input(f"\n1- peze K pou'w soti nan jwèt la\n2- nenpot lot touch pou'w kontinye  :")
     if kontinye.lower()=="k":
         break
     os.system("clear")
